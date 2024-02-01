8eaef8a53b7b2a1ec35f6398c703da034404d8e5
Paul Nuschke <65721341+plnii@users.noreply.github.com>
2024-02-01 09:52:39 -0500

    pr review fixes

--- run-code-examples/order-sum-products/index.js
+++ run-code-examples/order-sum-products/index.js
@@ -3,11 +3,13 @@
 */
 
 export default function main({order}) {
+  const titleToMatch = "test";
+
   // Get all line items whose title contains the word "test"
-  const items = order.lineItems.filter((lineItem) => lineItem.title.includes("test"));
+  const items = order.lineItems.filter((lineItem) => lineItem.title.includes(titleToMatch));
 
   // Sum the quantity of all line items that matches
-  const productCount = items.reduce((accum, currentValue) => accum + currentValue.quantity, 0);
+  const productCount = items.reduce((accum, currentItem) => accum + currentItem.quantity, 0);
 
   // Return true if there are any matching products
   const hasProduct = productCount > 0 ? true : false;
